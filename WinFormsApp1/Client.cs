da25bf76b4aca9d6624ee1767b64a4fbda0f60b1
Nikolnik24 <nikolaxdowork@gmail.com>
2026-03-12 10:54:04 +0300

    Создание класса Client

---
+++ WinFormsApp1/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WinFormsApp1
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public Client(string name, string email, string phone, string address)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Address = address;
+        }
+    }
+}
--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d636529fb2ce61cdf488038ca7485c4cc492b738")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58462f1ed33bc6f1c852676f9f140454d1dd9bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
